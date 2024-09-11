cc45120bd4a3570030b49f3e1f5c6b03db495331
Guille1309 <guillermina.abdullatifcoria@gmail.com>
2024-09-11 18:33:05 -0300

    Avances footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,20 @@
 import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+
+            <ul className="lista-footer">
+                <li className="lista-footer nombres">Abdullatif Guillermina</li>
+                <li className="lista-footer nombres">Paz Agustina</li>
+                <li className="lista-footer nombres">Selvas Camila</li>
+                <li className="logofooter"><img src="./img/logoTMDB.svg" alt="Logo TMDB"></img> </li>
+            </ul>
+
+        </footer>
+    )
+
+}
+
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,19 @@ import "./Header.css"
 
 function Header() {
     return (
-        <header > 
+        <header >
             <div className="logo">
-                <img className="imgHeader" src="./imgs/logo.png" alt="Logo JCG"></img>
+                <img className="imgHeader" src="./img/logo.png" alt="Logo JCG"></img>
             </div>
             <div>
                 <nav className="navListHeader">
                     <ul className="ulNav">
                         <li className="liNav"><Link to="/">Home</Link></li>
                         <li className="liNav"><Link to="/favoritos"> Favoritos</Link> </li>
-                        <li className="liNav"><Link to="/verTodas">Ver todas</Link></li>
+                        <li className="liVerTodas">Ver todas<ul>
+                            <li ><Link to="/verTodas?genre=Romance"> Series populares</Link></li>
+                            <li><Link to="//verTodas?genre=Terror"> Peliculas populares</Link></li>
+                        </ul></li>
                     </ul>
                 </nav>
             </div>
