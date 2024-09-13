e231d21081ed07af4c745e31b5de513905bb1f3e
camuxsel <cselvas@udesa.edu.ar>
2024-09-13 19:19:07 -0300

    css detallepeli

--- src/components/DetallePelicula/DetallePelicula.js
+++ src/components/DetallePelicula/DetallePelicula.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import InfoPeli from '../InfoPeli/InfoPeli';
 
+import './DetallePelicula.css'
+
 let APIkey = "b704c3c0bb664614cb7d33ce45f904f3";
 
 class DetallePelicula extends Component{
@@ -14,7 +16,7 @@ class DetallePelicula extends Component{
     }
 
     componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=${APIkey}`)
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=${APIkey}&language=es-MX`)
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
@@ -28,7 +30,7 @@ class DetallePelicula extends Component{
     render(){
         return(
             <React.Fragment>
-                <section>
+                <section className="sectionDetalle">
                     {this.state.pelicula.length === 0 ? 
                     <h3>Cargando...</h3> :
                     <InfoPeli pelicula={this.state.pelicula}/>}
--- src/components/InfoPeli/InfoPeli.js
+++ src/components/InfoPeli/InfoPeli.js
@@ -1,25 +1,28 @@
 import React from "react";
+import './InfoPeli.css';
 
 function InfoPeli(props){
     console.log(props);
     let pelicula = props.pelicula;
     return(
-        <section>
-            <img src={pelicula.backdrop_path} alt={`Poster de ${pelicula.original_title}`}/>
-            <h1>{pelicula.title}</h1>
-            <ul>
-                <li>Rating: {pelicula.vote_average}</li>
-                <li>Fecha de estreno: {pelicula.release_date}</li>
-                <li>Duración: {pelicula.runtime} min.</li>
-                <li>Géneros: 
-                    <ul>
-                        {pelicula.genres.map((genre) => <li>{genre.name}</li>)}
-                    </ul>
-                </li>
-            </ul>
-            <h2>Sinopsis</h2>
-            <p>{pelicula.overview}</p>
-            <p>Agregar a Favoritos</p>
+        <section className="infoDetalle">
+            <img className='imgDetalle' src={pelicula.backdrop_path} alt={`Poster de ${pelicula.original_title}`}/>
+            <h1 className="tituloDetalle">{pelicula.title}</h1>
+            <article className="contenedorDatosDetalle">
+                <ul className="listaDetalle">
+                    <li className="liDetalle">Rating: {pelicula.vote_average}</li>
+                    <li className="liDetalle">Fecha de estreno: {pelicula.release_date}</li>
+                    <li className="liDetalle">Duración: {pelicula.runtime} min.</li>
+                    <li className="liDetalle">Géneros: 
+                        <ul className="generosDetalle">
+                            {pelicula.genres.map((genre) => <li>{genre.name}</li>)}
+                        </ul>
+                    </li>
+                </ul>
+            </article>
+            <h2 className="subtituloDetalle">Sinopsis</h2>
+            <p className="sinopsisDetalle">{pelicula.overview}</p>
+            <p className="favoritos">Agregar a Favoritos</p>
         </section>
     )
 }
