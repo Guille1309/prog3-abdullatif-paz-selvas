fbfafb3fd54d047c6b69f52f6461aa999bb75468
camuxsel <cselvas@udesa.edu.ar>
2024-09-13 19:26:42 -0300

    viendo css

--- src/screens/NotFound/NotFound.js
+++ src/screens/NotFound/NotFound.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import './NotFound.css';
+
 function NotFound() {
     return(
         <React.Fragment>
             <Header/>
-            <h1>404 - Contenido inexistente</h1>
+            <h1 className="tituloNotFound">404 - Contenido inexistente</h1>
             <Footer/>
         </React.Fragment>
     )
