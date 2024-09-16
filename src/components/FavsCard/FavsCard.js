5bb7b68c3de07a0d5cb9e0efb987d12fda930f03
camuxsel <cselvas@udesa.edu.ar>
2024-09-16 18:02:14 -0300

    terminando css favs :)

--- src/components/FavsCard/FavsCard.js
+++ src/components/FavsCard/FavsCard.js
@@ -21,13 +21,14 @@ class FavsCard extends Component{
 
     render(){
         return(
-            <article className="infoPeli">
-                <Link to={`/peliculas/detalle/${this.state.pelicula.id}`}>
+            <React.Fragment>
+                <Link to={`/peliculas/detalle/${this.state.pelicula.id}`} className='linksFav'>
                     <img src={`https://image.tmdb.org/t/p/w500${this.state.pelicula.poster_path}`} alt={`Poster de ${this.state.pelicula.title}`} className="imgPeliFav"/>
                     <h2 className="tituloPeli">{this.state.pelicula.title}</h2>
                 </Link>
-                <button onClick={this.sacarFav(this.state.pelicula)} className="botonFavoritos">Eliminar de favoritos</button>
-            </article>
+                <button onClick={() => this.sacarFav(this.state.pelicula)} className="botonFavoritos">Eliminar de favoritos</button>
+            </React.Fragment>
+
         )
     }
 }
--- src/components/MostrarFavoritos/MostrarFavoritos.js
+++ src/components/MostrarFavoritos/MostrarFavoritos.js
@@ -10,8 +10,8 @@ function MostrarFavoritos(props) {
     return(
         <React.Fragment>
             {pelisFav.length === 0 ? 
-            <h1>Aún no tienes películas favoritas</h1> :
-            <h1>Películas favoritas</h1>}
+            <h1 className="tituloFavs">Aún no tienes películas favoritas</h1> :
+            <h1 className="tituloFavs">Películas favoritas</h1>}
             <ul className="mostrarFavs">
                 {pelisFav.map((peli) => <li className="listaMostrarFavs"><FavsCard infoPeli={peli}/></li>)}
             </ul>
